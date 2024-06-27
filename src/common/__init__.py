b0f249f660c9a786c8cc7d6448649819766b22fa
realcoding2003 <kevinpark@okyc.kr>
2024-06-27 11:43:17 +0900

    임포트 문으로 로거를 사용할 수 있도록 업데이트

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,6 +1,24 @@
-# src/common/__init__.py
-# Import common utilities for easier access
 from .utils import call_api, create_directory_if_not_exists
 from .logging_setup import setup_logging
+from .state import stop_event
 
-__all__ = ['call_api', 'create_directory_if_not_exists', 'setup_logging']
+import logging
+
+# Ensure logging is set up when this package is imported
+setup_logging()
+
+# Export common functions and loggers
+__all__ = [
+    'call_api',
+    'create_directory_if_not_exists',
+    'setup_logging',
+    'stop_event',
+    'general_logger',
+    'data_processor_logger',
+    'collect_all_data_logger'
+]
+
+# Export loggers
+general_logger = logging.getLogger('general')
+data_processor_logger = logging.getLogger('data_processor')
+collect_all_data_logger = logging.getLogger('collect_all_data')
