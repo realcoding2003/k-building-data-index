62ecdc859f8f1845a870561d5fc7a0665bf46d91
realcoding2003 <kevinpark@okyc.kr>
2024-06-28 08:53:03 +0900

    logging 모듈 정리

--- src/common/logging_setup.py
+++ src/common/logging_setup.py
@@ -19,22 +19,15 @@ def setup_logging():
     general_logger.addHandler(general_handler)
 
     # Data Processor logger
-    data_processor_logger = logging.getLogger('data_processor')
+    data_processor_logger = logging.getLogger('data_collector')
     data_processor_logger.setLevel(logging.INFO)
-    data_processor_handler = logging.FileHandler(os.path.join(log_dir, 'data_processor.log'))
+    data_processor_handler = logging.FileHandler(os.path.join(log_dir, 'data_collector.log'))
     data_processor_handler.setFormatter(log_formatter)
     data_processor_logger.addHandler(data_processor_handler)
 
     # Collect All Data logger
-    collect_all_data_logger = logging.getLogger('collect_all_data')
+    collect_all_data_logger = logging.getLogger('scripts')
     collect_all_data_logger.setLevel(logging.INFO)
-    collect_all_data_handler = logging.FileHandler(os.path.join(log_dir, 'collect_all_data.log'))
+    collect_all_data_handler = logging.FileHandler(os.path.join(log_dir, 'scripts.log'))
     collect_all_data_handler.setFormatter(log_formatter)
     collect_all_data_logger.addHandler(collect_all_data_handler)
-
-    # Add console handler to all loggers
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
-    general_logger.addHandler(console_handler)
-    data_processor_logger.addHandler(console_handler)
-    collect_all_data_logger.addHandler(console_handler)
