5dec4592272e2e37851b68ad8b44a04664dab66f
realcoding2003 <kevinpark@okyc.kr>
2024-06-27 11:45:37 +0900

    로그 이름 변경

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -11,14 +11,13 @@ setup_logging()
 __all__ = [
     'call_api',
     'create_directory_if_not_exists',
-    'setup_logging',
     'stop_event',
-    'general_logger',
-    'data_processor_logger',
-    'collect_all_data_logger'
+    'log_general',
+    'log_data_processor',
+    'log_collect_all_data'
 ]
 
 # Export loggers
-general_logger = logging.getLogger('general')
-data_processor_logger = logging.getLogger('data_processor')
-collect_all_data_logger = logging.getLogger('collect_all_data')
+log_general = logging.getLogger('general')
+log_data_processor = logging.getLogger('data_processor')
+log_collect_all_data = logging.getLogger('collect_all_data')
